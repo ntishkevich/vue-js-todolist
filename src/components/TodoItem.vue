f711be4acf23fc3cfa08f28735a72c926ded43ca
ntishkevich <Nikolai.Tishkevich@artezio.com>
2017-07-24 23:42:25 +0300

    vue todo list start

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="dropdown customSelect">
+    <ul class="dropdown-menu">
+      <li v-for="(option, index) in options" :key="index">
+        <a v-on:mousedown.prevent="select(option)">
+          {{ displayTextConte(option) }}
+        </a>
+      </li>
+      <li v-if="!options.length">
+
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  const todoItem = new Vue();
+
+</script>
+
+<style>
+
+</style>
